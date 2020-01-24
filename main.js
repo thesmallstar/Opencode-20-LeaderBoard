9c61968b9a193071f8c90a2f5b0e3b6dcbf53d51
untruly_noxius <chetangarg365@gmail.com>
2020-01-24 14:14:53 +0530

    Responsive

--- main.js
+++ main.js
@@ -27,8 +27,7 @@ function addToTable(arr) {
               var markup = "<tr><td><strong>"+ (i + 1) +"</strong></td><td><strong>" +'<img src="' +img+ '" alt="Italian Trulli" style="width:50px; height:50px;">' + "</strong></td><td><strong>" + "&nbsp;" + name + "</strong></td><td><strong>" + "&nbsp;" + points + "</strong></td></tr>";
             }
             else{*/
-              var markup = "<tr><td>"+ (i + 1) +"</strong></td><td><strong>" +'<img src="'+img+ '" alt="Italian Trulli" style="width:50px; height:50px;">' + "</td><td> " + "&nbsp;" + name + "</td><td> " + "&nbsp;" + points + "</td><td>" + "&nbsp;" + "<a href=search.html?q="+name+">"+"<img src=\"img/arrow.svg\" height=30px width=30px></a>" + "</td></tr>";
-
+              var markup = "<tr><td>"+ (i + 1) +"</strong></td><td><strong>" +'<img src="'+img+ '" alt="Italian Trulli" style="width:50px; height:50px;">' + "</td><td> " + "&nbsp;" + name + "</td><td> " + "&nbsp;" + points + "</td><td>" + "&nbsp;" + "<a href=search.html?q="+name+">"+"<img src=\"img/arrow.svg\" height=25em width=25em></a>" + "</td></tr>";
             //}
             $("table tbody").append(markup);
     }
@@ -50,7 +49,7 @@ function addTop5images(arr){
       var ids = ['first', 'second', 'third', 'fourth', 'fifth'];
       var div = document.getElementById("top5");
       div.className = "row table table-success table-bordered table-hover border border-dark rounded w-100";
-      div.style = "font-size: 85%; font-weight: bold; align-self: auto; column-width:auto; margin-bottom: 10px; padding-top: 10px; padding-bottom: 10px;"
+      div.style = "font-size: 0.8em; font-weight: bold; align-self: auto; column-width:auto; margin-bottom: 10px; padding-top: 10px; padding-bottom: 10px;"
       
       for(var i=0;i<5;i++)
       {
@@ -58,19 +57,28 @@ function addTop5images(arr){
          div.innerHTML = '';
          var img = document.createElement('img'); 
          img.src = arr[i].img; 
-         img.style.height = '150px';
-         img.style.width = '150px';
+         img.style.height = '5em';
+         img.style.width = '5em';
          div.appendChild(img);
+         var breakl = document.createElement('br'); 
+         div.appendChild(breakl);
          var idtoupper = ids[i].toUpperCase();
          var markup = document.createElement('html');
-         markup = idtoupper +"\t\t" + arr[i].username + " |" + arr[i].points + "|" ;
+         markup = idtoupper;
          div.append(markup);
-         var link = document.createElement('a');
+         var breakl2 = document.createElement('br'); 
+         div.appendChild(breakl2);
+         var markup2 = document.createElement('html');
+         markup2 = arr[i].username + " |" + arr[i].points + "|";
+         div.append(markup2);
+        var link = document.createElement('a');
          link.href = "search.html?q="+arr[i].username;
          var imgd = document.createElement('img'); 
          imgd.src = "https://marketplace.canva.com/MACq7KcSUB8/1/screen/canva-arrow-shapes-simple-icon-MACq7KcSUB8.svg"; 
-         imgd.style.height = '30px';
-         imgd.style.width = '30px';
+         imgd.style.height = '1em';
+         imgd.style.width = '1em';
+         var breakl3 = document.createElement('br'); 
+         div.appendChild(breakl3);
          link.appendChild(imgd);
          div.appendChild(link);
       }
